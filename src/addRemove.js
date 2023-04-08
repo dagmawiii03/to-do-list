4a118df76cb1103d688fe209fbebdd80a48f10d5
dagmawiii03 <dagmawi.yoha03@gmail.com>
2023-04-08 21:01:26 +0300

    solve eslint errror

--- src/addRemove.js
+++ src/addRemove.js
@@ -1,6 +1,5 @@
 import todoList from './TodoList.js';
 
-const todos = document.querySelector('.todos');
 const clear = document.createElement('li');
 clear.classList.add('clear');
 clear.innerHTML = 'Clear all completed';
@@ -35,41 +34,4 @@ export const add = (item) => {
     index: todoList.length + 1,
   });
   localStorage.setItem('todoList', JSON.stringify(todoList));
-//   const li = document.createElement('li');
-//   li.classList.add('list-field');
-//   li.id = todoList.length + 1;
-//   li.innerHTML = `
-// <div class="list-label">
-// <input type="checkbox" name="task" />
-// <input
-//   type="text"
-//   name="task"
-//   class="list-input"
-//   value="${item}"
-// />
-// </div>
-// `;
-//   const icon = document.createElement('span');
-//   icon.innerHTML = "<i class='fa-solid fa-ellipsis-vertical'></i>";
-//   li.appendChild(icon);
-
-//   icon.addEventListener(
-//     'click',
-//     () => icon.getAttribute('icon') === 'delete' && removeTask(currentItem),
-//   );
-
-//   li.addEventListener('click', () => {
-//     currentItem = li.id;
-//     const allInput = document.querySelectorAll('span');
-//     allInput.forEach((i) => {
-//       i.innerHTML = "<i class='fa-solid fa-ellipsis-vertical'></i>";
-//       i.setAttribute('icon', 'move');
-//       i.style.color = 'black';
-//     });
-//     icon.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
-//     icon.setAttribute('icon', 'delete');
-//     icon.style.color = 'red';
-//   });
-//   todos.appendChild(li);
-//   todos.appendChild(clear);
 };
